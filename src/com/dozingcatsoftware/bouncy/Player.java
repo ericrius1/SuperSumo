9cf32822b545ed5cd55235e77a65553386319698
Eric Levin <Ericrius1@gmail.com>
2013-02-25 14:19:53 -0700

    commit

--- src/com/dozingcatsoftware/bouncy/Player.java
+++ src/com/dozingcatsoftware/bouncy/Player.java
@@ -5,22 +5,21 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.CircleShape;
 import com.dozingcatsoftware.bouncy.elements.Box2DFactory;
 
 public class Player {
 	SpriteBatch batch;
-	Vector2 spritePosition = new Vector2();
-	Vector2 spritePreviousPosition = new Vector2();
 	private int width;
 	private int height;
 	private static final int REGION_COLUMNS = 3;
 	private static final int REGION_ROWS = 4;
 	private Texture playerTexture;
 	private float currentFrame = 0;
-	private float xSpeed = 20.0f;
-	private float ySpeed = 0.0f;
+	// private float xSpeed = 20.0f;
+	// private float ySpeed = 0.0f;
 	static final float FRAME_RATE = 5;
 	private float centerScreenY;
 	float screenHeight;
@@ -28,6 +27,11 @@ public class Player {
 	float touchRange;
 	Body body;
 	ScreenTranslationUtils screenUtils;
+	Vector2 worldTarget;
+	Vector3 touchPoint;
+	Vector3 worldPoint;
+
+	float attractStrength = 3.0f;
 
 	private static final int[] DIRECTION_TO_ANIMATION_MAP = {3, 1, 0, 2};
 
@@ -37,18 +41,14 @@ public class Player {
 		playerTexture = new Texture("data/player.png");
 		this.width = playerTexture.getWidth() / REGION_COLUMNS;
 		this.height = playerTexture.getHeight() / REGION_ROWS;
-		spritePosition.x = 10;
-		spritePosition.y = 10;
-		spritePreviousPosition = spritePosition;
 		screenHeight = Gdx.graphics.getHeight();
 		screenWidth = Gdx.graphics.getWidth();
 		touchRange = width * 4;
 		screenUtils = new ScreenTranslationUtils(30f);
+		touchPoint = new Vector3();
 
-		// Physics
-		Vector2 bodyPos = screenUtils.coordPixelsToWorld(spritePosition);
 		float box2dW = screenUtils.scalarPixelsToWorld(width / 2);
-		body = Box2DFactory.createCircle(Field.world, bodyPos.x, bodyPos.y, box2dW, false);
+		body = Box2DFactory.createCircle(Field.world, 10, 20, box2dW, false);
 		body.setBullet(true);
 
 	}
@@ -57,20 +57,21 @@ public class Player {
 		int srcX = (int)currentFrame * width;
 		int srcY = getAnimationRow() * height;
 		batch.begin();
-		batch.draw(playerTexture, spritePosition.x, spritePosition.y, width, height, srcX, srcY, width, height, false, false);
+		Vector2 spritePos = screenUtils.getBodyPixelCoord(body);
+		batch.draw(playerTexture, body.getPosition().x, body.getPosition().y, width, height, srcX, srcY, width, height, false,
+			false);
 		batch.end();
 		CircleShape shape = (CircleShape)body.getFixtureList().get(0).getShape();
-		Vector2 position = screenUtils.getBodyPixelCoord(body);
-		Bouncy.renderer.fillCircle(position.x, position.y, shape.getRadius(), 50, 200, 20);
+		Bouncy.renderer.fillCircle(body.getPosition().x, body.getPosition().y, shape.getRadius(), 50, 200, 20);
 
 		// If sprite is down, set the sprite's x/y coordinates
 		if (Gdx.input.isTouched()) {
 			float screenY = ScreenTranslationUtils.map(Gdx.input.getY(), screenHeight, 0, 0, screenHeight);
+			worldPoint = screenToViewport(Gdx.input.getX(), Gdx.input.getY());
+			attract(worldPoint.x, worldPoint.y);
 			if (isCollision(Gdx.input.getX(), screenY)) {
 
-				spritePosition.set(Gdx.input.getX(), screenY);
-				position = screenUtils.coordPixelsToWorld(Gdx.input.getX(), Gdx.input.getY());
-				body.setTransform(position, body.getAngle());
+				// spritePosition.set(Gdx.input.getX(), screenY);
 			}
 
 		}
@@ -79,10 +80,10 @@ public class Player {
 
 	public void update (float deltaTime) {
 
-		xSpeed = (spritePosition.x - spritePreviousPosition.x) / deltaTime;
-		ySpeed = (spritePreviousPosition.y - spritePosition.y) / deltaTime;
+// xSpeed = (spritePosition.x - spritePreviousPosition.x) / deltaTime;
+// ySpeed = (spritePreviousPosition.y - spritePosition.y) / deltaTime;
 
-		spritePreviousPosition = new Vector2(spritePosition.x, spritePosition.y);
+		// spritePreviousPosition = new Vector2(spritePosition.x, spritePosition.y);
 
 		// current frame = ++ currentFrame % 3
 		currentFrame += FRAME_RATE * deltaTime;
@@ -93,18 +94,41 @@ public class Player {
 
 	}
 
+	void applyForce (Vector2 force) {
+		body.applyForce(force, body.getWorldCenter());
+		// Use this if you want flock to be affected by gravity
+		// body.applyForce(new Vector2(force.x, Field.world.getGravity().y), body.getWorldCenter());
+	}
+
+	void attract (float x, float y) {
+		// worldTarget = screenUtil.coordPixelsToWorld(x , y);
+		worldTarget = new Vector2(x, y);
+		// first find the vector going from this body to the specified point
+		worldTarget = worldTarget.sub(body.getWorldCenter());
+		// Then scale vector to specified force
+		worldTarget = worldTarget.nor();
+		worldTarget = worldTarget.mul(attractStrength);
+		// Now apply it to the body's center of mass
+		applyForce(worldTarget);
+	}
+
 	private int getAnimationRow () {
-		double dirDouble = (Math.atan2(xSpeed, ySpeed) / (Math.PI / 2) + 2);
-		int direction = (int)Math.round(dirDouble) % REGION_ROWS;
-		return DIRECTION_TO_ANIMATION_MAP[direction];
+		// double dirDouble = (Math.atan2(xSpeed, ySpeed) / (Math.PI / 2) + 2);
+		// int direction = (int)Math.round(dirDouble) % REGION_ROWS;
+		// return DIRECTION_TO_ANIMATION_MAP[direction];
+		return 1;
 	}
 
 	private boolean isCollision (float x2, float y2) {
 
-		System.out.println("mouseY = " + y2);
-		System.out.println("playerY = " + spritePosition.y);
-		return (Math.abs(x2 - spritePosition.x) < touchRange && Math.abs(spritePosition.y - y2) < touchRange);
+		// return (Math.abs(x2 - spritePosition.x) < touchRange && Math.abs(spritePosition.y - y2) < touchRange);
+		return true;
 
+	}
+
+	public Vector3 screenToViewport (float x, float y) {
+		Field.cam.unproject(touchPoint.set(Gdx.input.getX(), Gdx.input.getY(), 0));
+		return touchPoint;
 	}
 
 }
